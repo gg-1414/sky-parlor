87f577e3d1327577447fef690ff806c458a6dea0
Gina Lee <gina@thesistesting.com>
2021-07-30 11:59:13 -0700

    at this point routing works. Remove old components

--- src/App.js
+++ src/App.js
@@ -1,58 +1,63 @@
 import React, { Component } from 'react';
-import LandingPage from './components/LandingPage';
-import Main from './components/Main';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+import Landing from './containers/Landing';
+import Home from './containers/Home';
+import Product from './containers/Product.js';
 import './App.scss';
 
-const apiUrl = 'https://boiling-gorge-97500.herokuapp.com';
+// const apiUrl = 'https://sky-parlor-admin.herokuapp.com';
+
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
       playlistEmbedCode: '',
-      showLandingPage: true,
     }
   }
 
-  fetchPlaylist = async () => {
-    const res = await fetch(apiUrl + '/playlist/recent');
-    const playlist = await res.json();
-    this.setState({ playlistEmbedCode: playlist.embedCode });
-    console.log('playlist',playlist)
-  };
+  // fetchPlaylist = async () => {
+  //   const res = await fetch(apiUrl + '/playlist/recent');
+  //   const playlist = await res.json();
+  //   this.setState({ playlistEmbedCode: playlist.embedCode });
+  //   console.log('playlist',playlist)
+  // };
 
-  renderPage = () => {
-    if (this.state.showLandingPage) {
-      return (
-        <LandingPage handleLandingPageClick={this.handleLandingPageClick} />
-      );
-    } else {
-      return (
-        <Main />
-      )
-    }
-  }
-
-  handleLandingPageClick = () => {
-    this.setState({ showLandingPage: false });
+  handleLandingClick = () => {
+    this.setState({ showLanding: false, showMainPage: true });
   }
 
   componentWillMount() {
-    this.fetchPlaylist();
-    console.log('this.playlistEmbedCode',this.state.playlistEmbedCode)
+    // this.fetchPlaylist();
+    // console.log('this.playlistEmbedCode',this.state.playlistEmbedCode)
   }
 
   render() {
-    const { playlistEmbedCode, showLandingPage } = this.state;
+    // const { playlistEmbedCode, showLanding, showMainPage } = this.state;
 
     return (
       <div className="App">
+        {/* <div className="playlist" dangerouslySetInnerHTML={{ __html: playlistEmbedCode }} /> */}
 
-        { this.renderPage() }
-
-        <div className="playlist" dangerouslySetInnerHTML={{ __html: playlistEmbedCode }} />
-    
-    </div>
+        <Router>
+          <Switch>
+            <Route exact path="/product">
+              <Product />
+            </Route>
+            <Route exact path="/home">
+              <Home />
+            </Route>
+            <Route path="/">
+              <Landing />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
     );
   }
 }
