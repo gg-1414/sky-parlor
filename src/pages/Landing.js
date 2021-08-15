c0ad2e29b71c82d3d7526095d8b5daecf2807cf3
Gina Lee <gina@thesistesting.com>
2021-08-15 11:57:51 -0700

    add react-responsive package, show image if on mobile

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,28 +1,33 @@
 import { Link } from 'react-router-dom';
+import { useMediaQuery } from 'react-responsive'
 import bgVideo from '../assets/sky-bg-spinning-logo.mp4';
 import bgVideoPlaceholder from '../assets/sky-bg-spinning-logo.png';
 import styles from '../styles/pages/Landing.module.scss';
 
 export default function Landing() {
+  const isMobile = useMediaQuery({ query: '(max-width: 768px)' });
+
   return (
     <div className="relative">
       <Link to="/home">
-        <div
-          className="object-cover m-0 h-screen w-full"
-          dangerouslySetInnerHTML={{
-            __html: `
-              <video id="landing-video" class="object-cover m-0 h-screen w-full" autoplay loop muted playsinline poster=${bgVideoPlaceholder}>
-                <source src=${bgVideo} type="video/mp4" />
-                Your browser does not support the video tag.
-              </video>
-              <script>
-                document.addEventListener("DOMContentLoaded", function() {
-                  const video = document.getElementById("landing-video");
-                  video.play();
-                })
-              </script>`,
-          }}
-        />
+        {isMobile && (
+          <div className="object-cover m-0 h-screen w-full">
+            <img src={bgVideoPlaceholder} alt="Background of logo and sky" className="object-cover m-0 h-screen w-full" />
+          </div>
+        )}
+
+        {!isMobile && (
+          <div
+            className="object-cover m-0 h-screen w-full"
+            dangerouslySetInnerHTML={{
+              __html: `
+                <video id="landing-video" class="object-cover m-0 h-screen w-full" autoplay loop muted playsinline poster=${bgVideoPlaceholder}>
+                  <source src=${bgVideo} type="video/mp4" />
+                  Your browser does not support the video tag.
+                </video>`,
+            }}
+          />
+        )}
       </Link>
       <div className={`${styles.label} absolute w-full px-4 top-0 right-0 flex items-center justify-between z-20"`}>
         <Link to="/admin" className="underline text-xs">Admin</Link>
